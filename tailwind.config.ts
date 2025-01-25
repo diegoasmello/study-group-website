7d3253823054373f99f68a145372b39b318dd147
Diego Mello <diegoasmello@gmail.com>
2025-01-25 04:13:48 -0300

    wip: config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,15 @@ export default {
           "Noto Color Emoji",
         ],
       },
+      colors: {
+        primary: {
+          lighter: "#EEEFF5",
+          light: "#525F9B",
+          DEFAULT: "#273782",
+          dark: "#1F2C68",
+          darker: "#192353",
+        },
+      },
     },
   },
   plugins: [],
