bebaf12baa19abf6317cb447291a63ef5b1866eb
Diego Mello <diegoasmello@gmail.com>
2025-01-31 01:11:09 -0300

    im just tired

--- app/components/form-fields/DateRangeInput.tsx
+++ app/components/form-fields/DateRangeInput.tsx
@@ -1,7 +1,14 @@
 import { FormControl, FormControlProps } from "./FormControl";
 import { TextInput } from "./TextInput";
 
-interface DateRangeInputProps extends Omit<FormControlProps, "children"> {}
+interface DateRangeInputProps
+  extends Omit<FormControlProps, "children">,
+    Omit<React.ComponentProps<"input">, "type"> {}
+
+// type DateRange = {
+//   startDate: Date;
+//   endDate: Date;
+// };
 
 export function DateRangeInput(props: DateRangeInputProps) {
   const { label } = props;
@@ -9,9 +16,9 @@ export function DateRangeInput(props: DateRangeInputProps) {
   return (
     <FormControl label={label}>
       <div className="flex items-center gap-8">
-        <TextInput name={"a"} placeholder="00/00/0000" type="date" />
+        <TextInput name={"a"} type="date" />
         <span className="text-gray-950 font-medium">até</span>
-        <TextInput name={"a"} placeholder="00/00/0000" type="date" />
+        <TextInput name={"a"} type="date" />
       </div>
     </FormControl>
   );
