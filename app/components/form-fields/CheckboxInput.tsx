77c19b64eabb81e925ce0ed912767b09833058ce
Diego Mello <diegoasmello@gmail.com>
2025-02-02 19:53:05 -0300

    checkbox

--- app/components/form-fields/CheckboxInput.tsx
+++ app/components/form-fields/CheckboxInput.tsx
@@ -1,23 +1,34 @@
 import { Checkbox } from "@headlessui/react";
 import { IconCheck } from "../icons";
 import { FormControlProps } from "./FormControl";
+import { useRef, useState } from "react";
 
 interface CheckboxInputProps
-  extends React.ComponentProps<"input">,
+  extends Omit<React.ComponentProps<"input">, "type" | "className" | "ref">,
     Omit<FormControlProps, "children" | "htmlFor"> {}
 
 export function CheckboxInput(props: CheckboxInputProps) {
-  const { name, label } = props;
+  const { label, ...rest } = props;
+
+  const [isChecked, setIsChecked] = useState(false);
 
   return (
-    <label className="flex items-center gap-2" htmlFor={name}>
+    <label className="flex items-center gap-2 cursor-pointer">
       <Checkbox
-        name={name}
+        checked={isChecked}
+        onChange={setIsChecked}
         className="h-[24px] w-[24px] group flex items-center justify-center rounded-md border border-gray-200 bg-white cursor-pointer transition hover:border-primary focus:shadow-focus data-[checked]:bg-primary data-[checked]:text-white data-[checked]:hover:bg-primary-darker"
       >
         <IconCheck className="hidden size-6 fill-black group-data-[checked]:block" />
       </Checkbox>
       {label && <span className="text-gray-950 font-medium">{label}</span>}
+      <input
+        type="checkbox"
+        className="hidden"
+        checked={isChecked}
+        onChange={(e) => setIsChecked(e.currentTarget.checked)}
+        {...rest}
+      />
     </label>
   );
 }
