793f5009b1d6c3702a42dabe0f1bb11376fed26d
Diego Mello <diegoasmello@gmail.com>
2025-02-17 17:43:01 -0300

    improve handle error

--- apps/web/app/utils/handle-errors.ts
+++ apps/web/app/utils/handle-errors.ts
@@ -1,5 +1,5 @@
 export function handleNotFound(
-  ...items: (object | number | string | null | undefined)[]
+  ...items: (object | number | string | boolean | null | undefined)[]
 ) {
   if (items.every((item) => !item)) {
     throw new Response(null, {
