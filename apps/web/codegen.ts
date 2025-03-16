b37f159e67ca9ef469b735db130a0b8c6f4d423b
Diego Mello <diegoasmello@gmail.com>
2025-03-16 16:09:32 -0300

    remove avoid optionals from codegen config

--- apps/web/codegen.ts
+++ apps/web/codegen.ts
@@ -17,9 +17,6 @@ const config: CodegenConfig = {
         "typescript-operations",
         "typescript-graphql-request",
       ],
-      config: {
-        avoidOptionals: true,
-      },
     },
   },
 };
